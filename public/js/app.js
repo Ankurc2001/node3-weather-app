f31a9ae588ee5930f9894b0e4560e9ba2109e8e5
Ankur chauhan <ankurc2001@gmail.com>
2020-04-22 17:52:30 +0530

    remove unnessary cosole.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('clint side java script is loaded')
-
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 =  document.querySelector('#msg-1')
