70344ff2ed824fba2354b99ada447abe5540b3c6
Ankur chauhan <ankurc2001@gmail.com>
2020-04-22 18:43:48 +0530

    setting app for horeku

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const hbs= require('hbs')
 const geocode = require('./utils/geocode')
 const forecaste = require('./utils/forecaste')
+const port = process.env.PORT || 3000
 
 const app = express()
 
@@ -94,6 +95,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('server is up on port 3000')
+app.listen(port,()=>{
+    console.log('server is up on port '+port)
 }) 
